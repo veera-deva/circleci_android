9d6a359461ad67268be3654592fddaada11423a1
veera <Veerachamy.Devarajan@marks-and-spencer.com>
2023-05-30 16:06:44 +0530

    circle ci testing

---
+++ app/src/test/java/com/circleci/samples/todoapp/ExampleUnitTest.kt
@@ -0,0 +1,23 @@
+package com.circleci.samples.todoapp
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+// Sample failing test :)
+//    @Test
+//    fun addition_shouldFail() {
+//        assertEquals(5, 2 + 2)
+//    }
+}
---
+++ macrobenchmark/src/main/java/com/circleci/samples/todoapp/macrobenchmark/StartupBenchmarks.kt
@@ -0,0 +1,33 @@
+package com.circleci.samples.todoapp.macrobenchmark
+
+import androidx.benchmark.macro.StartupTimingMetric
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+const val TARGET_PACKAGE = "com.circleci.samples.todoapp"
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class SampleStartupBenchmark {
+    @get:Rule
+    val benchmarkRule = MacrobenchmarkRule()
+
+    @Test
+    fun startup() = benchmarkRule.measureRepeated(
+        packageName = TARGET_PACKAGE,
+        metrics = listOf(StartupTimingMetric()),
+        iterations = 5,
+        setupBlock = {
+            // Press home button before each run to ensure the starting activity isn't visible.
+            pressHome()
+        }
+    ) {
+        // starts default launch activity
+        startActivityAndWait()
+    }
+}
